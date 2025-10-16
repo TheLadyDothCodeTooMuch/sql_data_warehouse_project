446487331389175371a4f1801a75e77e7b9713f7
Deborah Ike <chikadeborahike@gmail.com>
2025-10-16 23:15:18 +0100

    Format SQL WHERE clause for better readability

--- scripts/silver/data_cleanup.sql
+++ scripts/silver/data_cleanup.sql
@@ -123,4 +123,5 @@ SELECT
         sls_quantity,
         sls_price
 FROM silver.crm_sales_details
-WHERE sls_sales != sls_quantity * sls_price OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL OR sls_sales = 0 OR sls_quantity = 0 OR sls_price = 0;
+WHERE sls_sales != sls_quantity * sls_price OR sls_sales IS NULL OR sls_quantity IS NULL 
+    OR sls_price IS NULL OR sls_sales = 0 OR sls_quantity = 0 OR sls_price = 0;
