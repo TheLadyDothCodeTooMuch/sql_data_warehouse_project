b91b44c2db3b8d10ffb522f42ca3850a704bc4fd
Deborah Ike <chikadeborahike@gmail.com>
2025-10-17 13:34:48 +0100

    Refactor SQL data cleanup script for clarity and accuracy
    Updated comments for clarity and corrected SQL queries to reference the correct schema. Added additional checks for data integrity in the crm_sales_details and erp_cust_az12 tables.

--- scripts/silver/data_cleanup.sql
+++ scripts/silver/data_cleanup.sql
@@ -1,7 +1,8 @@
 -- ======================================================================================
 -- Run these codes for crm_cust_info after the data cleanup to test if all is as should be
 -- ======================================================================================
--- Checks for duplicate or multiple logs of the primary key (customer ID). The expected result is 0. Do this for the cst_id, which is supposed to be a unique value.
+-- Checks for duplicate or multiple logs of the primary key (customer ID). The expected result is 0. Do this for the cst_id, 
+-- which is supposed to be a unique value.
 SELECT
     cst_id,
     COUNT(*) AS "Count"
@@ -29,7 +30,8 @@ FROM silver.crm_cust_info;
 -- Run these codes for crm_prd_info after the data cleanup to test if all is as should be
 -- ======================================================================================
 
--- Checks for duplicate or multiple logs of the primary key. The expected result is 0. Do this for the cst_id, which is supposed to be a unique value.
+-- Checks for duplicate or multiple logs of the primary key. The expected result is 0. Do this for the cst_id, 
+-- which is supposed to be a unique value.
 SELECT
     prd_id
 FROM silver.crm_prd_info
@@ -113,11 +115,12 @@ WHERE 0 IN (sls_order_dt, sls_ship_dt,
 -- Convert sls_due_dt values of 0 to NULL for records where sls_due_dt is 0, to handle invalid or missing dates. The expected result is 0
 SELECT DISTINCT
         sls_due_dt
-FROM bronze.crm_sales_details
+FROM silver.crm_sales_details
 WHERE sls_due_dt = 0 OR LEN(sls_due_dt) <> 8
 ;
 
--- Identifies records where the total sales figure does not match the calculated value (quantity * price), or where the data is NULL. The expected result is 0
+-- Identifies records where the total sales figure does not match the calculated value (quantity * price), or where the data is NULL. 
+-- The expected result is 0
 SELECT 
         sls_sales,
         sls_quantity,
@@ -125,3 +128,50 @@ SELECT
 FROM silver.crm_sales_details
 WHERE sls_sales != sls_quantity * sls_price OR sls_sales IS NULL OR sls_quantity IS NULL 
     OR sls_price IS NULL OR sls_sales = 0 OR sls_quantity = 0 OR sls_price = 0;
+
+
+-- ===================================================================================================
+-- Run these codes for silver.crm_sales_details after the data cleanup to test if all is as should be
+-- ===================================================================================================
+
+-- Run these codes after the data cleanup to test if all is as should be.
+
+--Checks for duplicate or multiple logs of the primary key. The expected result is 0. Do this for the CID, 
+-- which is supposed to be a unique value.
+SELECT
+    CID
+FROM silver.erp_cust_az12
+GROUP BY CID
+HAVING COUNT(*) > 1 OR CID IS NULL
+;
+
+--Checks for unwanted spaces in CID and GEN. The expected result is 0. Use for the firstname and lastname columns
+SELECT
+        GEN
+FROM silver.erp_cust_az12
+WHERE GEN != TRIM(GEN)
+;
+
+-- Checks for duplicate or multiple logs of a customer's ID after ordering from latest to earliest. The expected result is 0. 
+-- Do this for the cst_id, which is supposed to be a unique value.
+SELECT
+*
+FROM (SELECT
+        *,
+        ROW_NUMBER() OVER(PARTITION BY CID ORDER BY bdate DESC) AS flag_last
+        FROM silver.erp_cust_az12) AS t
+WHERE flag_last != 1;
+
+-- Check for invalid dates i.e birth days in the long past or the future. The expected result is 0
+SELECT
+        BDATE
+FROM silver.erp_cust_az12
+WHERE BDATE < '1925-01-01' or BDATE > GETDATE()
+;
+
+-- Check all genders listed. The expected result should be only Male, Female or n/a
+SELECT DISTINCT
+        gen
+FROM silver.erp_cust_az12
+--WHERE gen != TRIM(gen)
+;
